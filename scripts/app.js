726a43f7b1ecbd4dffa7919c499f79b08e48b803
Simon Huber <simipro@simipromac.local>
2018-02-11 17:28:48 +0100

    remove all

--- scripts/app.js
+++
@@ -1,8 +0,0 @@
-var span = document.getElementsByTagName('span')[0];
-span.textContent = 'the amazing';
-span.style.display = 'inline';
-// create a new element, style it, and append it to the DOM
-var loadTime = document.createElement('div');
-loadTime.textContent = 'You loaded this page on: ' + new Date();
-loadTime.style.color = 'blue';
-document.body.appendChild(loadTime)
